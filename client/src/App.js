94c49cb10768c95526c2ffcc1ccaafa291aad1ac
user.name: "Angelo Guiao <user.email: "angeloguiao.gci@gmail.com>
2023-11-19 22:56:48 +0800

    Linking pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,49 @@
 import Dashboard from './modules/Dashboard/Dashboard';
 import Form from './modules/Form/index';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const isLoggedIn = localStorage.getItem('user:token') !== null || true;
+
+  if (!isLoggedIn) {
+    // If not logged in, redirect to the sign-in page
+    return <Navigate to="/user/sign_in" />;
+  }else if(isLoggedIn && ['/sign_in', '/sign_up'].includes(window.location.pathname)){
+    console.log('Object :>>');
+    return <Navigate to={'/'} />
+  }
+
+  // If logged in, render the children
+  return children;
+};
 
 function App() {
   return (
     <Routes>
-    <Route path='/' element={<Dashboard />} />
-    <Route path='/user/sign_in' element={<Form isSignInPage={true} />} />
-    <Route path='/user/sign_up' element={<Form isSignInPage={false} />}  />
-  </Routes>
-  
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/user/sign_in"
+        element={
+        <ProtectedRoute>
+          <Form isSignInPage={true} />
+        </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/user/sign_up"
+        element={ <ProtectedRoute>
+          <Form isSignInPage={true} />
+        </ProtectedRoute>
+      }
+      />
+    </Routes>
   );
 }
 
